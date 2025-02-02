a03e1e35f3a5acd0c0f6dccdad7f0bcf18fb080e
Rojin Bijukchhe <ro_jin25@icloud.com>
2025-02-01 20:01:04 -0600

    ADD | createTourRating and Exception. Happy coding :)

--- explorecali-jpa/src/main/java/com/example/explorecalijpa/web/TourRatingController.java
+++ explorecali-jpa/src/main/java/com/example/explorecalijpa/web/TourRatingController.java
@@ -1,9 +1,20 @@
 package com.example.explorecalijpa.web;
 
+import java.util.NoSuchElementException;
+
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.example.explorecalijpa.business.TourRatingService;
+
+import jakarta.validation.Valid;
+
 
 @RestController
 @RequestMapping(path = "/tours/{tourId}/ratings")
@@ -14,4 +25,21 @@ public class TourRatingController {
         this.tourRatingService = tourRatingService;
     }
 
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createTourRating(
+    @PathVariable(value = "tourId") int tourId, 
+        @RequestBody @Valid RatingDto ratingDto) {
+            tourRatingService.createNew(
+                tourId, 
+                ratingDto.getCustomerId(), 
+                ratingDto.getScore(), 
+                ratingDto.getComment());
+    }
+    
+    @ExceptionHandler(NoSuchElementException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String return404(NoSuchElementException exception){
+        return exception.getMessage();
+    }
 }
