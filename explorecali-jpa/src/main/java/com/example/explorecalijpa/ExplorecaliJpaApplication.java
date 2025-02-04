09f9dc6dad519915efb9d8cb15e212d3db3b18a2
Rojin Bijukchhe <ro_jin25@icloud.com>
2025-02-04 16:40:19 -0600

    UPDATE | Json file read successfully.

--- explorecali-jpa/src/main/java/com/example/explorecalijpa/ExplorecaliJpaApplication.java
+++ explorecali-jpa/src/main/java/com/example/explorecalijpa/ExplorecaliJpaApplication.java
@@ -1,13 +1,17 @@
 package com.example.explorecalijpa;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 import com.example.explorecalijpa.business.TourPackageService;
 import com.example.explorecalijpa.business.TourService;
@@ -16,34 +20,55 @@ import com.example.explorecalijpa.model.Region;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
 @SpringBootApplication
 public class ExplorecaliJpaApplication implements CommandLineRunner {
-	private final String TOUR_IMPORT_FILE = "ExploreCalifornia.json";
+    private final String TOUR_IMPORT_FILE = "ExploreCalifornia.json";
 
-	@Autowired
-	private TourPackageService tourPackageService;
+    @Bean
+    public OpenAPI swaggerHeader() {
+        return new OpenAPI()
+            .info((new Info())
+            .description("Services for the Explore California Relational Database.")
+            .title(StringUtils.substringBefore(getClass().getSimpleName(), "$"))
+            .version("3.0.0"));
+    }
+    
 
-	@Autowired
-	private TourService tourService;
+    @Autowired
+    private TourPackageService tourPackageService;
 
-	public static void main(String[] args) {
-		SpringApplication.run(ExplorecaliJpaApplication.class, args);
-	}
+    @Autowired
+    private TourService tourService;
 
-	@Override
-	public void run(String... args) throws Exception{
-		createTourAllPackage();
-		System.out.println("Persisted Packages = " + tourPackageService.total());
+    public static void main(String[] args) {
 
-		// createToursFromFile(TOUR_IMPORT_FILE);
-		// System.out.println("Presisted Tours = " + tourService.total());
-		
+        SpringApplication.run(ExplorecaliJpaApplication.class, args);
+    }
 
-	}
+    @Override
+    public void run(String... args) throws Exception {
+        createTourAllPackages();
+        System.out.println("Persisted Packages = " + tourPackageService.total());
+        createToursFromFile(TOUR_IMPORT_FILE);
+        System.out.println("Persisted Tours = " + tourService.total());
+       
+        /********* CHALLENGES **********/
+        // System.out.println("\n\nEasy Tours");
+        // tourService.lookupByDifficulty(Difficulty.Easy).forEach(System.out::println);
 
-	public void createTourAllPackage(){
-		tourPackageService.createTourPackage("BC", "Backpack Cal");
-		tourPackageService.createTourPackage("CC", "California Calm");
+        // System.out.println("\n\nBackpack Cali Tours");
+        // tourService.lookupByPackage("BC").forEach(System.out::println);
+    }
+
+    /**
+     * Initialize all the known tour packages
+     */
+    private void createTourAllPackages() {
+        tourPackageService.createTourPackage("BC", "Backpack Cal");
+        tourPackageService.createTourPackage("CC", "California Calm");
         tourPackageService.createTourPackage("CH", "California Hot springs");
         tourPackageService.createTourPackage("CY", "Cycle California");
         tourPackageService.createTourPackage("DS", "From Desert to Sea");
@@ -51,11 +76,13 @@ public class ExplorecaliJpaApplication implements CommandLineRunner {
         tourPackageService.createTourPackage("NW", "Nature Watch");
         tourPackageService.createTourPackage("SC", "Snowboard Cali");
         tourPackageService.createTourPackage("TC", "Taste of California");
-	}
+    }
 
-	private void createToursFromFile(String fileToImport) throws IOException {
-        TourFromFile.read(fileToImport).forEach(t -> 
-			tourService.createTour(
+    /**
+     * Create tour entities from an external file
+     */
+    private void createToursFromFile(String fileToImport) throws IOException {
+        TourFromFile.read(fileToImport).forEach(t -> tourService.createTour(
                 t.packageName(),
                 t.title(),
                 t.description(),
@@ -68,23 +95,19 @@ public class ExplorecaliJpaApplication implements CommandLineRunner {
                 Region.findByLabel(t.region())));
     }
 
-	record TourFromFile(
-		String packageName, 
-		String title, 
-		String description,
-		String blurb, 
-		Integer price, 
-		String length, 
-		String bullets,
-		String keywords, 
-		String difficulty, 
-		String region ) {
-			static List<TourFromFile> read(String fileToImport) throws IOException {
-				return new ObjectMapper().readValue(new File(fileToImport),
-						new TypeReference<List<TourFromFile>>() {
-						});
+    /*
+     * Helper to import ExploreCali.json
+     */
+    record TourFromFile(String packageName, String title, String description,
+            String blurb, Integer price, String length, String bullets,
+            String keywords, String difficulty, String region) {
+       static List<TourFromFile> read(String fileToImport) throws IOException {
+			try (InputStream is = ExplorecaliJpaApplication.class.getResourceAsStream("/" + fileToImport)) {
+				if (is == null) {
+					throw new FileNotFoundException("File not found in classpath: " + fileToImport);
+				}
+				return new ObjectMapper().readValue(is, new TypeReference<List<TourFromFile>>() {});
 			}
-	}
-
-
+		}
+    }
 }
